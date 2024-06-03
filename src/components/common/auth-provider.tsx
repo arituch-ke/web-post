8add7141d7c4d7866e015707ccaa9db2a2c28ebb
anon-kae <anon.ksl42@gmail.com>
2024-06-03 23:59:34 +0700

    chore: Add new provider components for theme and Redux state management

---
+++ src/components/common/auth-provider.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch } from "@/store";
+import { usePathname, useRouter } from "next/navigation";
+import { getCurrentUser, userState } from "@/store/silces/userSlice";
+import { useSelector } from "react-redux";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AuthProvider({ children }: Props) {
+  const router = useRouter();
+  const path = usePathname();
+  const dispatch = useAppDispatch();
+  const { isAuthenticated, accessToken } = useSelector(userState);
+  const [loading, setLoading] = useState(true);
+
+  // Initialize guards
+  // const initializeGuards = async () => {
+  //   const isAuthPath = ["/login", "/register"].includes(path);
+  //   const hasAccess = isAuthenticated && accessToken;
+
+  // if (hasAccess && isAuthPath) {
+  //   await dispatch(getCurrentUser());
+  //   router.replace("/");
+  // } else if (!hasAccess && !isAuthPath) {
+  //   router.replace("/login");
+  // } else {
+  //   setLoading(false);
+  // }
+  // };
+
+  useEffect(() => {
+    const initializeGuards = async () => {
+      const isAuthPath = ["/login", "/register"].includes(path);
+      const hasAccess = isAuthenticated && accessToken;
+
+      if (hasAccess && isAuthPath) {
+        await dispatch(getCurrentUser());
+        router.replace("/");
+      } else if (!hasAccess && !isAuthPath) {
+        router.replace("/login");
+      } else {
+        setLoading(false);
+      }
+    };
+
+    initializeGuards();
+  }, [isAuthenticated, accessToken, path, router, dispatch]);
+
+  if (loading) {
+    return null;
+  }
+
+  return children;
+}
