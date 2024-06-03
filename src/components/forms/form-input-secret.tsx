688e642b950c90843589cfd9874e1de252091ac2
anon-kae <anon.ksl42@gmail.com>
2024-06-04 00:04:45 +0700

    refactor: Add FormInputSecret and FormInputText components for form inputs

---
+++ src/components/forms/form-input-secret.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Input } from "@nextui-org/react";
+import { IPropFormInput } from "@/types";
+import { Controller } from "react-hook-form";
+
+export default function FormInputSecret({
+  name,
+  control,
+  label,
+  className,
+  size = "sm",
+  formState,
+  isRequired = true,
+}: Omit<IPropFormInput, "type">) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const IconWrapper = ({ children }: { children: React.ReactNode }) => (
+    <div className="material-symbols-outlined text-2xl text-default-400 pointer-events-none">
+      {children}
+    </div>
+  );
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Input
+          type={isVisible ? "text" : "password"}
+          label={label}
+          variant="bordered"
+          isRequired={isRequired}
+          isInvalid={!!formState?.errors[name]}
+          size={size}
+          {...field}
+          errorMessage={formState.errors[name]?.message}
+          className={className}
+          endContent={
+            <button
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
+            >
+              {isVisible ? (
+                <IconWrapper>visibility_off</IconWrapper>
+              ) : (
+                <IconWrapper>visibility</IconWrapper>
+              )}
+            </button>
+          }
+        />
+      )}
+    />
+  );
+}
