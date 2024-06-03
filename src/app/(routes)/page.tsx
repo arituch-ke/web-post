9818d73c213e36f336a982e3afbbc527080deb79
anon-kae <anon.ksl42@gmail.com>
2024-06-04 00:00:22 +0700

    refactor: Add new page component for home page

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useAppDispatch } from "@/store";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to your new project.</p>
+    </div>
+  );
+}
