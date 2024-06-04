86bb9bacafb93de6dd909e68137c201e92a072f7
anon-kae <anon.ksl42@gmail.com>
2024-06-05 01:33:19 +0700

    refactor: Remove unused "use client" import in layout component

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,6 +1,6 @@
+"use client";
 import React from "react";
 import { Navbar } from "@/components/navbar";
-import { ToastContainer } from "react-toastify";
 
 type Props = {
   children: React.ReactNode;
@@ -9,9 +9,8 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className="relative flex flex-col h-screen">
-      <ToastContainer autoClose={5000} />
       <Navbar />
-      <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+      <main className="container overflow-y-auto mx-auto max-w-7xl px-6 flex-grow">
         {children}
       </main>
     </div>
